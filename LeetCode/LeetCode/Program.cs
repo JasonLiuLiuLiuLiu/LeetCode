fc02198b0a19ec8af42a676bfd531f00ecaeef55
Zhenyu,Liu <liuzhenyulive@live.com>
2019-02-22 01:24:06 +0800

    solve two sum

---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new _001().TwoSum(new int[] { -3, 2, 3 }, 0);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
