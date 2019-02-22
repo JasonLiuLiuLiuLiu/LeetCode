4fb262ba3ead0f7d42c4bd2396b89db9b57ed3cd
Zhenyu,Liu <liuzhenyulive@live.com>
2019-02-23 00:32:59 +0800

    add unit test

---
+++ Java/LeetCode/src/test/MainTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Main Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>���� 23, 2019</pre>
+ */
+public class MainTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: main(String[] args)
+     */
+    @Test
+    public void testMain() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+    /**
+     * Method: twoSum(int[] nums, int target)
+     */
+    @Test
+    public void testTwoSum() throws Exception {
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = Main.getClass().getMethod("twoSum", int[].class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/
+    }
+
+} 
